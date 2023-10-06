33999050f406f55d82c200605e4f023fec666e17
Jody Nguyen <baoanjnguyen@gmail.com>
2023-10-06 12:29:44 -0400

    updated organization of classes and functions, needs ChangeCalc bugfix

--- capstone/src/main/java/com/techelevator/VendingMachineActions.java
+++ capstone/src/main/java/com/techelevator/VendingMachineActions.java
@@ -1,9 +1,223 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.*;
 import java.io.File;
 
 public class VendingMachineActions {
+
+    //Initialize Scanner for User Input
+    public static Scanner userInput = new Scanner(System.in);
+
+    //Method to display the Main Menu options
+    public static void displayMainMenuOptions(Map<String, StuffedAnimal> inventoryMap, BigDecimal balance) throws VendingMachineException, InterruptedException {
+        //Display main menu options
+        System.out.println("(1) Display Vending Machine Items");
+        System.out.println("(2) Purchase");
+        System.out.println("(3) Exit");
+        System.out.println("Type the corresponding number to make your selection: ");
+
+        //Ask user to enter a number for choice
+        String mainMenuChoice = userInput.nextLine();
+
+        if (mainMenuChoice.equals("1")) {
+            VendingMachineActions.displayVendingMachineItems(inventoryMap, balance);
+
+        } else if (mainMenuChoice.equals("2")) {
+            displayPurchaseMenu(inventoryMap, balance);
+
+        } else if (mainMenuChoice.equals("3")) {
+            System.out.println("Thank you for using this vending machine.");
+
+        } else {
+            System.out.println("Ya done goofed. Try again.");
+            displayMainMenuOptions(inventoryMap, balance);
+        }
+    }
+
+    //Method to output the contents of inventoryMap in order
+    public static void displayVendingMachineItems(Map<String,StuffedAnimal> inventoryMap, BigDecimal balance) throws VendingMachineException, InterruptedException {
+        System.out.println(inventoryMap.get("A1").getProductCode() + ") " + inventoryMap.get("A1").getProductName() + " $" + inventoryMap.get("A1").getProductPrice() + " Quantity: " + inventoryMap.get("A1").getQuantityInStock());
+        System.out.println(inventoryMap.get("A2").getProductCode() + ") " + inventoryMap.get("A2").getProductName() + " $" + inventoryMap.get("A2").getProductPrice() + " Quantity: " + inventoryMap.get("A2").getQuantityInStock());
+        System.out.println(inventoryMap.get("A3").getProductCode() + ") " + inventoryMap.get("A3").getProductName() + " $" + inventoryMap.get("A3").getProductPrice() + " Quantity: " + inventoryMap.get("A3").getQuantityInStock());
+        System.out.println(inventoryMap.get("A4").getProductCode() + ") " + inventoryMap.get("A4").getProductName() + " $" + inventoryMap.get("A4").getProductPrice() + " Quantity: " + inventoryMap.get("A4").getQuantityInStock());
+
+        System.out.println(inventoryMap.get("B1").getProductCode() + ") " + inventoryMap.get("B1").getProductName() + " $" + inventoryMap.get("B1").getProductPrice() + " Quantity: " + inventoryMap.get("B1").getQuantityInStock());
+        System.out.println(inventoryMap.get("B2").getProductCode() + ") " + inventoryMap.get("B2").getProductName() + " $" + inventoryMap.get("B2").getProductPrice() + " Quantity: " + inventoryMap.get("B2").getQuantityInStock());
+        System.out.println(inventoryMap.get("B3").getProductCode() + ") " + inventoryMap.get("B3").getProductName() + " $" + inventoryMap.get("B3").getProductPrice() + " Quantity: " + inventoryMap.get("B3").getQuantityInStock());
+        System.out.println(inventoryMap.get("B4").getProductCode() + ") " + inventoryMap.get("B4").getProductName() + " $" + inventoryMap.get("B4").getProductPrice() + " Quantity: " + inventoryMap.get("B4").getQuantityInStock());
+
+        System.out.println(inventoryMap.get("C1").getProductCode() + ") " + inventoryMap.get("C1").getProductName() + " $" + inventoryMap.get("C1").getProductPrice() + " Quantity: " + inventoryMap.get("C1").getQuantityInStock());
+        System.out.println(inventoryMap.get("C2").getProductCode() + ") " + inventoryMap.get("C2").getProductName() + " $" + inventoryMap.get("C2").getProductPrice() + " Quantity: " + inventoryMap.get("C2").getQuantityInStock());
+        System.out.println(inventoryMap.get("C3").getProductCode() + ") " + inventoryMap.get("C3").getProductName() + " $" + inventoryMap.get("C3").getProductPrice() + " Quantity: " + inventoryMap.get("C3").getQuantityInStock());
+        System.out.println(inventoryMap.get("C4").getProductCode() + ") " + inventoryMap.get("C4").getProductName() + " $" + inventoryMap.get("C4").getProductPrice() + " Quantity: " + inventoryMap.get("C4").getQuantityInStock());
+
+        System.out.println(inventoryMap.get("D1").getProductCode() + ") " + inventoryMap.get("D1").getProductName() + " $" + inventoryMap.get("D1").getProductPrice() + " Quantity: " + inventoryMap.get("D1").getQuantityInStock());
+        System.out.println(inventoryMap.get("D2").getProductCode() + ") " + inventoryMap.get("D2").getProductName() + " $" + inventoryMap.get("D2").getProductPrice() + " Quantity: " + inventoryMap.get("D2").getQuantityInStock());
+        System.out.println(inventoryMap.get("D3").getProductCode() + ") " + inventoryMap.get("D3").getProductName() + " $" + inventoryMap.get("D3").getProductPrice() + " Quantity: " + inventoryMap.get("D3").getQuantityInStock());
+        System.out.println(inventoryMap.get("D4").getProductCode() + ") " + inventoryMap.get("D4").getProductName() + " $" + inventoryMap.get("D4").getProductPrice() + " Quantity: " + inventoryMap.get("D4").getQuantityInStock());
+
+//        System.out.println("---");
+//        Thread.sleep(1000);
+
+        displayMainMenuOptions(inventoryMap, balance);
+    }
+
+    public static void displayPurchaseMenu(Map<String, StuffedAnimal> inventoryMap, BigDecimal balance) {
+
+        System.out.println("---");
+        System.out.println("");
+        System.out.println("Current Money Provided: $" + balance);
+        System.out.println("");
+        System.out.println("(1) Feed Money");
+        System.out.println("(2) Select Product");
+        System.out.println("(3) Finish Transaction");
+        System.out.println("");
+        System.out.println("---");
+        System.out.println("Type the corresponding number to make your selection: ");
+        String purchaseMenuChoice = userInput.nextLine();
+
+        if (purchaseMenuChoice.equals("1")) {
+            VendingMachineActions.feedMoney(inventoryMap, balance);
+
+        } else if (purchaseMenuChoice.equals("2")) {
+            displayPurchaseList(inventoryMap, balance);
+
+        } else if (purchaseMenuChoice.equals("3")) {
+            calculateChange(balance);
+        } else {
+            System.out.println("Oopsie ya made a poopsie.");
+            displayPurchaseMenu(inventoryMap, balance);
+        }
+    }
+
+    public static void feedMoney(Map<String, StuffedAnimal> inventoryMap, BigDecimal balance) {
+        Scanner readMoneyInput = new Scanner(System.in);
+
+        System.out.println("Please input the whole dollar amount you wish to deposit: ");
+        System.out.println("Note: this machine only accepts $1, $5, and $10 bills");
+
+        System.out.print("$");
+        String moneyInput = readMoneyInput.nextLine();
+
+        balance = balance.add(new BigDecimal(moneyInput));
+
+        displayPurchaseMenu(inventoryMap, balance); //update the Purchase Menu display w/ new balance
+
+    }
+
+    public static void displayPurchaseList(Map<String, StuffedAnimal> inventoryMap, BigDecimal balance) {
+        System.out.println(inventoryMap.get("A1").getProductCode() + ") " + inventoryMap.get("A1").getProductName() + " $" + inventoryMap.get("A1").getProductPrice() + " Quantity: " + inventoryMap.get("A1").getQuantityInStock());
+        System.out.println(inventoryMap.get("A2").getProductCode() + ") " + inventoryMap.get("A2").getProductName() + " $" + inventoryMap.get("A2").getProductPrice() + " Quantity: " + inventoryMap.get("A2").getQuantityInStock());
+        System.out.println(inventoryMap.get("A3").getProductCode() + ") " + inventoryMap.get("A3").getProductName() + " $" + inventoryMap.get("A3").getProductPrice() + " Quantity: " + inventoryMap.get("A3").getQuantityInStock());
+        System.out.println(inventoryMap.get("A4").getProductCode() + ") " + inventoryMap.get("A4").getProductName() + " $" + inventoryMap.get("A4").getProductPrice() + " Quantity: " + inventoryMap.get("A4").getQuantityInStock());
+
+        System.out.println(inventoryMap.get("B1").getProductCode() + ") " + inventoryMap.get("B1").getProductName() + " $" + inventoryMap.get("B1").getProductPrice() + " Quantity: " + inventoryMap.get("B1").getQuantityInStock());
+        System.out.println(inventoryMap.get("B2").getProductCode() + ") " + inventoryMap.get("B2").getProductName() + " $" + inventoryMap.get("B2").getProductPrice() + " Quantity: " + inventoryMap.get("B2").getQuantityInStock());
+        System.out.println(inventoryMap.get("B3").getProductCode() + ") " + inventoryMap.get("B3").getProductName() + " $" + inventoryMap.get("B3").getProductPrice() + " Quantity: " + inventoryMap.get("B3").getQuantityInStock());
+        System.out.println(inventoryMap.get("B4").getProductCode() + ") " + inventoryMap.get("B4").getProductName() + " $" + inventoryMap.get("B4").getProductPrice() + " Quantity: " + inventoryMap.get("B4").getQuantityInStock());
+
+        System.out.println(inventoryMap.get("C1").getProductCode() + ") " + inventoryMap.get("C1").getProductName() + " $" + inventoryMap.get("C1").getProductPrice() + " Quantity: " + inventoryMap.get("C1").getQuantityInStock());
+        System.out.println(inventoryMap.get("C2").getProductCode() + ") " + inventoryMap.get("C2").getProductName() + " $" + inventoryMap.get("C2").getProductPrice() + " Quantity: " + inventoryMap.get("C2").getQuantityInStock());
+        System.out.println(inventoryMap.get("C3").getProductCode() + ") " + inventoryMap.get("C3").getProductName() + " $" + inventoryMap.get("C3").getProductPrice() + " Quantity: " + inventoryMap.get("C3").getQuantityInStock());
+        System.out.println(inventoryMap.get("C4").getProductCode() + ") " + inventoryMap.get("C4").getProductName() + " $" + inventoryMap.get("C4").getProductPrice() + " Quantity: " + inventoryMap.get("C4").getQuantityInStock());
+
+        System.out.println(inventoryMap.get("D1").getProductCode() + ") " + inventoryMap.get("D1").getProductName() + " $" + inventoryMap.get("D1").getProductPrice() + " Quantity: " + inventoryMap.get("D1").getQuantityInStock());
+        System.out.println(inventoryMap.get("D2").getProductCode() + ") " + inventoryMap.get("D2").getProductName() + " $" + inventoryMap.get("D2").getProductPrice() + " Quantity: " + inventoryMap.get("D2").getQuantityInStock());
+        System.out.println(inventoryMap.get("D3").getProductCode() + ") " + inventoryMap.get("D3").getProductName() + " $" + inventoryMap.get("D3").getProductPrice() + " Quantity: " + inventoryMap.get("D3").getQuantityInStock());
+        System.out.println(inventoryMap.get("D4").getProductCode() + ") " + inventoryMap.get("D4").getProductName() + " $" + inventoryMap.get("D4").getProductPrice() + " Quantity: " + inventoryMap.get("D4").getQuantityInStock());
+
+        System.out.println("Please enter the product code for the item you wish to purchase: ");
+        String productChoice = userInput.nextLine();
+
+        //Check if Product Code exists
+        if (inventoryMap.containsKey(productChoice)) {
+
+            StuffedAnimal cheese = inventoryMap.get(productChoice);
+
+            //Check if there is any quantity of item left
+            if (cheese.getQuantityInStock() > 0) {
+
+                //Check if user has enough $ to purchase item
+                if (balance.compareTo(cheese.getProductPrice()) >= 0) {
+
+                    inventoryMap.get(productChoice).quantityInStock = inventoryMap.get(productChoice).quantityInStock - 1;
+                    balance = balance.subtract(cheese.getProductPrice());
+
+                    System.out.println(cheese.getAnimalMessage());
+                    //Timer stuff
+
+                    System.out.println("You purchased: " + cheese.getProductName() + " for $" + cheese.getProductPrice() + " and have $" + balance + " remaining.");
+
+                    displayPurchaseMenu(inventoryMap, balance);
+
+                } else {
+                    System.out.println("Insufficient funds. Please make another selection or deposit additional funds: ");
+                    displayPurchaseMenu(inventoryMap, balance);
+                }
+
+            } else {
+                System.out.println("This item is out of stock! Please make another selection: ");
+                displayPurchaseMenu(inventoryMap, balance);
+            }
+        } else {
+            System.out.println("This is not a valid product code! Please make another selection: ");
+            displayPurchaseMenu(inventoryMap, balance);
+        }
+
+    }
+
+    public static void calculateChange(BigDecimal change){
+        BigDecimal quarters = new BigDecimal(0);
+        BigDecimal nickels = new BigDecimal(0);
+        BigDecimal dimes = new BigDecimal(0);
+        String changeStatement = "";
+
+        if(change.remainder(new BigDecimal(.25)).compareTo(BigDecimal.ZERO) == 0 || (change.compareTo(BigDecimal.valueOf(.24))) == 1){
+            quarters = quarters.add(change.divide(BigDecimal.valueOf(.25)));
+            change = change.subtract(quarters.multiply(BigDecimal.valueOf(.25)));
+
+            if(change.remainder(new BigDecimal(.10)).compareTo(BigDecimal.ZERO) == 0 || (change.compareTo(BigDecimal.valueOf(.09))) == 1){
+                dimes = dimes.add(change.divide(BigDecimal.valueOf(.10)));
+                change = change.subtract(dimes.multiply(BigDecimal.valueOf(.10)));
+
+                if(change.remainder(new BigDecimal(.05)).compareTo(BigDecimal.ZERO) == 0 || (change.compareTo(BigDecimal.valueOf(.04))) == 1){
+                    nickels = nickels.add(change.divide(BigDecimal.valueOf(.05)));
+                    change = change.subtract(nickels.multiply(BigDecimal.valueOf(.05)));
+                }
+            }
+            BigInteger nickelsInt = nickels.toBigInteger();
+            BigInteger dimesInt = dimes.toBigInteger();
+            BigInteger quartersInt = quarters.toBigInteger();
+            changeStatement = "Your change is " + quartersInt + " quarters, " + dimesInt + " dimes, and " + nickelsInt + " nickels!";
+        }
+        else if(change.compareTo(BigDecimal.valueOf(.25)) == -1){
+            dimes = dimes.add(change.divide(BigDecimal.valueOf(.10)));
+            change = change.subtract(dimes.multiply(BigDecimal.valueOf(.10)));
+            if(change.remainder(new BigDecimal(.05)).compareTo(BigDecimal.ZERO) == 0 || (change.compareTo(BigDecimal.valueOf(.04))) == 1){
+                nickels = nickels.add(change.divide(BigDecimal.valueOf(.05)));
+                change = change.subtract(nickels.multiply(BigDecimal.valueOf(.05)));
+            }
+            BigInteger nickelsInt = nickels.toBigInteger();
+            BigInteger dimesInt = dimes.toBigInteger();
+            BigInteger quartersInt = quarters.toBigInteger();
+            changeStatement = "Your change is " + quartersInt + " quarters, " + dimesInt + " dimes, and " + nickelsInt + " nickels!";
+        }
+        else if(change.compareTo(BigDecimal.valueOf(.10)) == -1){
+            if(change.remainder(new BigDecimal(.05)).compareTo(BigDecimal.ZERO) == 0 || (change.compareTo(BigDecimal.valueOf(.04))) == 1){
+                nickels = nickels.add(change.divide(BigDecimal.valueOf(.05)));
+                change = change.subtract(nickels.multiply(BigDecimal.valueOf(.05)));
+            }
+            BigInteger nickelsInt = nickels.toBigInteger();
+            BigInteger dimesInt = dimes.toBigInteger();
+            BigInteger quartersInt = quarters.toBigInteger();
+            changeStatement = "Your change is " + quartersInt + " quarters, " + dimesInt + " dimes, and " + nickelsInt + " nickels!";
+        }
+        System.out.println(changeStatement);
+    }
 
 
 
--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -17,19 +17,17 @@ public class VendingMachineCLI {
 	//Prepare Inventory file for reading
 	Scanner inventoryInput = new Scanner(inventoryFile);
 
-	//Initialize Scanner for User Input
-	private final Scanner userInput = new Scanner(System.in);
-
 	//Initialize Map to hold inventory objects
 	public Map<String, StuffedAnimal> inventoryMap = new HashMap<String, StuffedAnimal>();
 
-
+	//Initialize the balance
+	BigDecimal balance = BigDecimal.valueOf(0);
 
 	public VendingMachineCLI() throws FileNotFoundException {
 
 	}
 
-	public void run() throws VendingMachineException {
+	public void run() throws VendingMachineException, InterruptedException {
 		//While loop to iterate through each line of the inventory list
 		while(inventoryInput.hasNextLine()){
 			//Split each line into multiple parts, saved into an array
@@ -43,57 +41,13 @@ public class VendingMachineCLI {
 			inventoryMap.put(toy.getProductCode(), toy);
 		}
 
-		//Main Menu
-
-		//Display main menu options
-		System.out.println("(1) Display Vending Machine Items");
-		System.out.println("(2) Purchase");
-		System.out.println("(3) Exit");
-		System.out.println("Type the corresponding number to make your selection: ");
-
-
-		//Ask user to enter a number for choice
-		String mainMenuChoice = userInput.nextLine();
-
-		if (mainMenuChoice.equals("1")) {
-			displayVendingMachineItems(inventoryMap);
-		}
-
-//		} else if (mainMenuChoice.equals("2")) {
-//
-//		} else if (mainMenuChoice.equals("3")); {
-//
-//		} else {
-//			throw new VendingMachineException("You did not input a valid selection!");
-//		}
-
+		//Display Main Menu
+		VendingMachineActions.displayMainMenuOptions(inventoryMap, balance);
 	}
 
-	public static void main(String[] args) throws FileNotFoundException, VendingMachineException {
+	public static void main(String[] args) throws FileNotFoundException, VendingMachineException, InterruptedException {
 		VendingMachineCLI cli = new VendingMachineCLI();
 		cli.run();
 	}
 
-	//Method to output the contents of inventoryMap in order
-	public void displayVendingMachineItems(Map<String,StuffedAnimal> inventoryMap) {
-		System.out.println(inventoryMap.get("A1").getProductCode() + ") " + inventoryMap.get("A1").getProductName() + " $" + inventoryMap.get("A1").getProductPrice() + " Quantity: " + inventoryMap.get("A1").getQuantityInStock());
-		System.out.println(inventoryMap.get("A2").getProductCode() + ") " + inventoryMap.get("A2").getProductName() + " $" + inventoryMap.get("A2").getProductPrice() + " Quantity: " + inventoryMap.get("A2").getQuantityInStock());
-		System.out.println(inventoryMap.get("A3").getProductCode() + ") " + inventoryMap.get("A3").getProductName() + " $" + inventoryMap.get("A3").getProductPrice() + " Quantity: " + inventoryMap.get("A3").getQuantityInStock());
-		System.out.println(inventoryMap.get("A4").getProductCode() + ") " + inventoryMap.get("A4").getProductName() + " $" + inventoryMap.get("A4").getProductPrice() + " Quantity: " + inventoryMap.get("A4").getQuantityInStock());
-
-		System.out.println(inventoryMap.get("B1").getProductCode() + ") " + inventoryMap.get("B1").getProductName() + " $" + inventoryMap.get("B1").getProductPrice() + " Quantity: " + inventoryMap.get("B1").getQuantityInStock());
-		System.out.println(inventoryMap.get("B2").getProductCode() + ") " + inventoryMap.get("B2").getProductName() + " $" + inventoryMap.get("B2").getProductPrice() + " Quantity: " + inventoryMap.get("B2").getQuantityInStock());
-		System.out.println(inventoryMap.get("B3").getProductCode() + ") " + inventoryMap.get("B3").getProductName() + " $" + inventoryMap.get("B3").getProductPrice() + " Quantity: " + inventoryMap.get("B3").getQuantityInStock());
-		System.out.println(inventoryMap.get("B4").getProductCode() + ") " + inventoryMap.get("B4").getProductName() + " $" + inventoryMap.get("B4").getProductPrice() + " Quantity: " + inventoryMap.get("B4").getQuantityInStock());
-
-		System.out.println(inventoryMap.get("C1").getProductCode() + ") " + inventoryMap.get("C1").getProductName() + " $" + inventoryMap.get("C1").getProductPrice() + " Quantity: " + inventoryMap.get("C1").getQuantityInStock());
-		System.out.println(inventoryMap.get("C2").getProductCode() + ") " + inventoryMap.get("C2").getProductName() + " $" + inventoryMap.get("C2").getProductPrice() + " Quantity: " + inventoryMap.get("C2").getQuantityInStock());
-		System.out.println(inventoryMap.get("C3").getProductCode() + ") " + inventoryMap.get("C3").getProductName() + " $" + inventoryMap.get("C3").getProductPrice() + " Quantity: " + inventoryMap.get("C3").getQuantityInStock());
-		System.out.println(inventoryMap.get("C4").getProductCode() + ") " + inventoryMap.get("C4").getProductName() + " $" + inventoryMap.get("C4").getProductPrice() + " Quantity: " + inventoryMap.get("C4").getQuantityInStock());
-
-		System.out.println(inventoryMap.get("D1").getProductCode() + ") " + inventoryMap.get("D1").getProductName() + " $" + inventoryMap.get("D1").getProductPrice() + " Quantity: " + inventoryMap.get("D1").getQuantityInStock());
-		System.out.println(inventoryMap.get("D2").getProductCode() + ") " + inventoryMap.get("D2").getProductName() + " $" + inventoryMap.get("D2").getProductPrice() + " Quantity: " + inventoryMap.get("D2").getQuantityInStock());
-		System.out.println(inventoryMap.get("D3").getProductCode() + ") " + inventoryMap.get("D3").getProductName() + " $" + inventoryMap.get("D3").getProductPrice() + " Quantity: " + inventoryMap.get("D3").getQuantityInStock());
-		System.out.println(inventoryMap.get("D4").getProductCode() + ") " + inventoryMap.get("D4").getProductName() + " $" + inventoryMap.get("D4").getProductPrice() + " Quantity: " + inventoryMap.get("D4").getQuantityInStock());
-	}
 }
